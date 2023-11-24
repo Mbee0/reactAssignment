1fc8ca0a482c8edf2e73fdb53616d39821b71a11
Mbee0 <mbee@cpp.edu>
2023-11-24 11:22:19 -0800

    changing the assignment number

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Homepage() {
     return (
         <div>
-            <h1>This is the homepage for Assignment #5</h1>
+            <h1>This is the homepage for Assignment #6</h1>
             <p>By: Megan Bee<br /></p>
             <h2> If you would like to go somewhere else:</h2>
             <Link to="/tictactoe">Tic-Tac-Toe Game</Link>
